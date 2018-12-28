12572ed2fdb8b4af2a962a72caf3e5f1045c40e4
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2018-12-28 04:44:02 -0200

    logout & middleware

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+//run de middleware isLoggedIn first then go to secret
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 });
 
@@ -87,6 +88,19 @@ app.post("/login", passport.authenticate("local", {
     failureRedirect: "/login"
 }) ,function(req, res){
 });
+
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
