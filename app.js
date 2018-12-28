be9a4c7176ccf2766e17f96cf831d43fdb7896d6
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2018-12-28 02:39:20 -0200

    sign up

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true })
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 //run as a function and pass some arguments, have to pass 3 diferent options 
 app.use(require("express-session")({
@@ -30,7 +31,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-
+//--------------------------------------
+//ROUTES
+//--------------------------------------
 
 
 app.get("/", function(req, res){
@@ -39,6 +42,34 @@ app.get("/", function(req, res){
 
 app.get("/secret", function(req, res){
     res.render("secret");
+});
+
+//--------------------------------------
+// Auth Routes
+//--------------------------------------
+
+//show sign up form
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+//handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+                                //pass the user object that we wanna create and then pass the password separaly[
+                                //and then if everything goes well that will return a new user that has everything inside of it (the username and de hash password as well)
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }else{
+            //that line will log the user in, it will take care of everything in the session. -> Store the correct information and run the serialize previusly defined on line 30/31
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secret");
+            });
+        }
+    });
 });
 
 
